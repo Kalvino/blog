d1a8bf46133af424332807d92e14f63dec68c02a
Kalvino <calvodira@gmail.com>
2013-06-09 05:49:22 +0300

    Finish user actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607222157) do
+ActiveRecord::Schema.define(:version => 20130609021227) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20130607222157) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
